c80c2ee72111b4219cb7ea591cf0957c0034becf
rooster7713 <57484764+rooster7713@users.noreply.github.com>
2019-12-26 10:42:18 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,1 @@
-$('#js-game-modal').on("mousedown mouseup", function(e) {
-    alert(e.type + " event fired at coords: " + e.pageX + ", " + e.pageY);
-    });
-    
-    x_coord = 1;
-    y_coord = 1;
-    
-    var e = jQuery.Event( "mousedown", { pageX: x_coord, pageY: y_coord } );
-    $('#js-game-modal').trigger(e);
-    
-    // execute more code
-    x_coord = 255;
-    y_coord = 255;
-    
-    var e = jQuery.Event( "mouseup", { pageX: x_coord, pageY: y_coord } );
-    $('#js-game-modal').trigger(e);
+alert("hdfldhlf");
